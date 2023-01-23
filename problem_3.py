85dbbabe1b9e1f3e0317ace310c4f30955b94ebf
christian-the-brewer <brewer.christiang@gmail.com>
2023-01-23 10:33:40 -0500

    solves problem 3

--- problem_3.py
+++ problem_3.py
@@ -4,5 +4,39 @@
 #What is the largest prime factor of the number 600851475143 ?
 
 
+def prime_factors(number):
+    limit = number
+    i = 1
+    factors = []
+    while i < limit:
+        if number % i == 0:
+            factor = int(number / i)
+            factors.append(i)
+            factors.append(factor)
+            limit = factor
+            # print(f"{i}, {factor}")
+        i += 1
+    # for num in factors:
+    #     print(num)
+    #check for prime 
+    factors.sort(reverse=True)
+    for num in factors:
+        if check_prime(num):
+            return num
+
+def check_prime(num):
+    if num % 2 == 0 or num % 3 == 0:
+        return False
+    elif num == 2 or num == 3:
+        return False
+    elif num == 1:
+        return False
+    for i in range(5, int(num ** 0.5)):
+        if num % i == 0:
+            return False
+
+    return True
+
+print(prime_factors(600851475143))
 
 
